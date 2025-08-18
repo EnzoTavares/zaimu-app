1e7cf6200efe8707271e9cf0799c756b4c848f74
Enzo Tavares <enzotavares2309@gmail.com>
2025-08-18 20:24:29 -0300

    Other changes

--- zaimu-backend/src/main/java/br/com/zaimu/backend/model/to/LoginParameters.java
+++ zaimu-backend/src/main/java/br/com/zaimu/backend/model/to/LoginParameters.java
@@ -10,7 +10,7 @@ public class LoginParameters {
     @Email(message = "O email deve ser válido.")
     private String email;
 
-    private String nickname;
+//    private String nickname;
 
     @NotBlank(message = "A senha é obrigatória")
     private String password;
